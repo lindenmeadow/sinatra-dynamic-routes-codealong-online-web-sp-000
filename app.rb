d6d30b0f8e678ffba942f8348b7c1be8ef74f7e2
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-10-17 20:20:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,6 +19,9 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-
+    num1 = params[:num1].to_i
+    num2 = params[:num2].to_i
+    answer = num1 * num2
+    answer
   end
 end
